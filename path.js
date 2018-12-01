a1ae99b3b27fd075c624aa2206ded1a8f4326386
Matteo Michelotti <matteo.michelotti@outlook.com>
2018-12-01 09:22:24 -0600

    Add Path class

---
+++ path.js
@@ -0,0 +1,30 @@
+class Path {
+    constructor() {
+        this.nodes = [];
+        this.radius = 15;
+    }
+
+    addNode(node) {
+        this.nodes.push(createVector(node.x, node.y));
+    }
+
+    getNodes() {
+        return this.nodes;
+    }
+
+    isOnPath(distance) {
+        return distance <= this.radius;
+    }
+
+    display() {
+        strokeWeight(this.radius * 2);
+        stroke(235);
+        for (let i = 0; i < this.nodes.length - 1; i++)
+            line(this.nodes[i].x, this.nodes[i].y, this.nodes[i + 1].x, this.nodes[i + 1].y);
+
+        strokeWeight(2);
+        stroke(150);
+        for (let i = 0; i < this.nodes.length - 1; i++)
+            line(this.nodes[i].x, this.nodes[i].y, this.nodes[i + 1].x, this.nodes[i + 1].y);
+    }
+}
